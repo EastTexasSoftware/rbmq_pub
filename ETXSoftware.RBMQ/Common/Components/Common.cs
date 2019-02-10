1c8d2a5c9cd221d0c525fc8ad110c71521aded2a
Stephen Miller <stephen.alan.miller@gmail.com>
2019-02-10 16:04:07 -0600

    Initial Commit

---
+++ ETXSoftware.RBMQ/Common/Components/Common.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Components
+{
+    public class Common
+    {
+        public const string ExchageName = "dev.exchange";
+        public const string QueueName = "dev.queue";
+        public const string RoutingKey = "dev.routing";
+    }
+}
